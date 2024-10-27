6239c2f70ec87f649620bd6c6dae42bd6b13fac2
mkruszyna <michal.kruszyna@gmail.com>
2024-10-27 20:49:38 +0100

    feat: refactor of methoods + unify prints and doctrings

--- Devices/device.py
+++ Devices/device.py
@@ -31,120 +31,136 @@ class Device(ABC):
         self.device_type = device_type
         self.connected = False
 
-    def connect_to_device(self):
+    def connect_to_device(self) -> None:
         """
-        Connect to the device
-        :return:
+        Connects to the device if compatible with the current class.
         """
-        print(f"{Fore.YELLOW}Connecting to device_id {self.device_id}")
-        if self.device_type != self.__class__.__name__.lower():
-            print(f"{Fore.RED}Error while connecting to device_id {self.device_id}")
-            raise NotCompatibleDevice
-        else:
-            print(f"{Fore.GREEN}Connected to device_id {self.device_id}")
-            self.connected = True
-        
+        print(f"{Fore.YELLOW}Connecting to device {self.device_id}...")
 
-    def disconnect_from_device(self):
+        if self.device_type != self.__class__.__name__.lower():
+            print(f"{Fore.RED}Connection error: Device {self.device_id} is not compatible.")
+            raise NotCompatibleDevice
+
+        self.connected = True
+        print(f"{Fore.GREEN}Successfully connected to device {self.device_id}.")      
+
+    def disconnect_from_device(self) -> None:
         """
-        Disconnect from the device
-        :return:
+        Disconnects from the device if currently connected.
         """
-        print(f"{Fore.YELLOW}Disconnecting from device_id {self.device_id}")
+        print(f"{Fore.YELLOW}Disconnecting from device {self.device_id}...")
+
         if self.connected:
             self.connected = False
-            print(f"{Fore.GREEN}Succesfully disconected from device_id {self.device_id}")
+            print(f"{Fore.GREEN}Successfully disconnected from device {self.device_id}.")
         else:
-            print(f"{Fore.YELLOW}device_id {self.device_id} was not connected")
+            print(f"{Fore.YELLOW}Device {self.device_id} was not connected.")
 
     def turn_on_off(self, new_state: str) -> None:
         """
-        Turn the device on or off.
+        Updates the device's power status to the specified state.
 
-        This method loads the JSON data from the file, iterates through the devices,
-        and updates the 'power' status of the device with the matching device_id to the new state.
-        It also updates the 'last_updated' timestamp for the device and saves the modified JSON data back to the file.
+        Loads the JSON data, finds the device by `device_id`, updates its 'power' status,
+        modifies the 'last_updated' timestamp, and saves the updated JSON data.
 
-        :param new_state: The new power state to set for the device ('on' or 'off').
+        Args:
+            new_state (str): The new power state to set for the device ('on' or 'off').
         """
         json_data = load_json()
+
         for device in json_data['devices']:
             if device['device_id'] == self.device_id:
                 device['status']['power'] = new_state
+                self.modify_last_updated(json_data)
+                print(f"{Fore.GREEN}Device {self.device_id} power set to {new_state}.")
                 break
-        self.modify_last_updated(json_data)
 
     def reboot(self) -> bool:
         """
-        Reboot the device.
+        Reboots the device by turning it off and then on, updating the 'last_updated' timestamp.
 
-        This method turns the device off and then back on, updating the 'last_updated' timestamp.
+        Returns:
+            bool: True if reboot was successful, False otherwise.
         """
         if not self.connected:
-            print(f"{Fore.RED}Cant perform reboot. Not connected to device_id {self.device_id}")
+            print(f"{Fore.RED}Cannot perform reboot. Device {self.device_id} is not connected.")
             return False
-        print(f"{Fore.YELLOW}Rebooting device_id {self.device_id}")
+
+        print(f"{Fore.YELLOW}Rebooting device {self.device_id}...")
+        
         self.turn_on_off("off")
         self.turn_on_off("on")
-        print(f"{Fore.GREEN}device_id {self.device_id} back online")
+        
+        print(f"{Fore.GREEN}Device {self.device_id} is back online.")
         return True
 
     def get_status(self) -> str | None:
+        """
+        Retrieves the status of the device if connected.
+
+        Returns:
+            str | None: The status of the device in JSON format if connected, None otherwise.
+        """
         if not self.connected:
-            print(f"{Fore.RED}Cant get status. Not connected to device_id {self.device_id}")
-            return
-        print(f"{Fore.GREEN}Getting status for device_id {self.device_id}")
+            print(f"{Fore.RED}Cannot get status. Device {self.device_id} is not connected.")
+            return None
+
+        print(f"{Fore.GREEN}Getting status for device {self.device_id}")
+
         json_data = load_json()
-        for device in json_data['devices']:
-            if device['device_id'] == self.device_id:
-                return f"{Fore.BLUE}{json.dumps(device['status'], indent=4)}"
+        device_status = next(
+            (device['status'] for device in json_data['devices'] if device['device_id'] == self.device_id),
+            None
+        )
+
+        if device_status is not None:
+            return f"{Fore.BLUE}{json.dumps(device_status, indent=4)}"
+        else:
+            print(f"{Fore.RED}Status not found for device {self.device_id}.")
+            return None
 
     def change_device_name(self, name: str) -> bool:
         """
-        Change device name
+        Changes the name of the device if connected.
 
-        This checks if user is connected to device if so, method loads the JSON data from the file, iterates through the devices,
-        and updates the 'name' parameter of the device with the matching device_id.
-        It also updates the 'last_updated' timestamp for the device and saves the modified JSON data back to the file.
-        If user is not connected to device, 
+        This method checks if the user is connected to the device, then loads the JSON data, finds the device by ID,
+        and updates the 'name' attribute. It also updates the 'last_updated' timestamp and saves the modified JSON data.
 
+        Args:
+            name (str): The new name for the device.
 
-        :param name: New name for the device.
-
-        :return: bool True if name was changed, otherwise False
+        Returns:
+            bool: True if the name was successfully changed, False otherwise.
         """
         if not self.connected:
-            print(f"{Fore.RED}Cant perform change_device_name. Not connected to device_id {self.device_id}")
+            print(f"{Fore.RED}Cannot change device name. Device {self.device_id} is not connected.")
             return False
-        changed = False
+
         json_data = load_json()
+        
         for device in json_data['devices']:
             if device['device_id'] == self.device_id:
                 device['name'] = name
-                changed = True
-                break
-        if changed:      
-            self.modify_last_updated(json_data)
-            print(f"{Fore.GREEN}Changing name of device_id {self.device_id} succeeded, new {name = }")
-            return True
-        else:
-            print(f"{Fore.RED}Changing name of device_id {self.device_id} failed")
-            return False
+                self.modify_last_updated(json_data)
+                print(f"{Fore.GREEN}Device name changed successfully. New name: {name}")
+                return True
+
+        print(f"{Fore.RED}Failed to change device name. Device {self.device_id} not found.")
+        return False
 
     def modify_last_updated(self, json_data) -> None:
         """
-        Update the 'last_updated' timestamp for the device.
+        Updates the 'last_updated' timestamp for the device in the JSON data.
 
-        This method iterates through the devices in the provided JSON data,
-        finds the device with the matching device_id, and updates its 'last_updated'
-        field with the current date and time.
+        Finds the device with the matching `device_id` and updates its 'last_updated' 
+        field with the current date and time, then saves the updated JSON data.
 
-        :param json_data: The JSON data containing device information.
-        :return: The updated JSON data with the modified 'last_updated' field.
+        Args:
+            json_data (dict): The JSON data containing device information.
         """
         for device in json_data['devices']:
             if device['device_id'] == self.device_id:
                 device['last_updated'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                save_json(json_data, FILE_PATH)
                 break
-        save_json(json_data, FILE_PATH)
 
