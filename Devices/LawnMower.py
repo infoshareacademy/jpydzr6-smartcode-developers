342e233c18417eb156595f159c1eba60ac9264f7
Rafal Szabat <rafalszabat2@gmail.com>
2024-11-17 19:47:57 +0100

    Update LawnMower.py
    deleted connect to device

--- Devices/LawnMower.py
+++ Devices/LawnMower.py
@@ -5,14 +5,14 @@ class LawnMower(Device):
     def __init__(self, device_id: str, device_type: str):
         super().__init__(device_id, device_type)
 
-    def connect_to_device(self) -> None:
+    '''def connect_to_device(self) -> None:
         """
         Connect to the lawn mower device.
         """
         if self.device_type != "LawnMower":
             raise NotCompatibleDevice("Device type is not compatible.")
         else:
-            print(f"Connected to Lawn Mower {self.device_id}")
+            print(f"Connected to Lawn Mower {self.device_id}")'''
 
     def load_device_info(self) -> dict:
         """
