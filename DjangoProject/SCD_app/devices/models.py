593c13af7ae4041c903f5e6c9ba048c5a2345aa3
IwonaAnna2024 <pinezzka@yahoo.com>
2025-01-19 22:28:33 +0100

    version at the end of today's classes

--- DjangoProject/SCD_app/devices/models.py
+++ DjangoProject/SCD_app/devices/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Device(models.Model):
+
 # Create your models here.
+    name = models.CharField(max_length=255, null=False)
+    connected = models.BooleanField(default=False)
+    brand = models.CharField(max_length=100, null=True)
+    last_updated = models.DateTimeField(auto_now=True)
--- DjangoProject/SCD_app/devices/views.py
+++ DjangoProject/SCD_app/devices/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Device
+
+def index(request):
+    qs = Device.objects.all()
+    device = qs.first()
+    response = ""
+
+    for device in qs:
+        response += f"{device.name}, {device.brand}, {device.connected}, {device.last_updated}"
+
+    return HttpResponse(response)
 
 # Create your views here.
