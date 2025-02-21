c72674a7895f380cf3d4c248e7e0132b1577387c
IwonaAnna2024 <pinezzka@yahoo.com>
2025-02-21 21:25:49 +0100

    update

--- DjangoProject/SCD_app/users/urls.py
+++ DjangoProject/SCD_app/users/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('users/dashboard', views.dashboard),
-    path('log_out/', include('users.urls', namespace='log_out')),
-    path('change_password/', include('users.urls', namespace='change_password')),
+    path('', views.dashboard),
+    # path('log_out/', log_out.views, name='logout'),
+    # path('change_password/', change_password.viws, namespace='change_password')),
     path('device_form/', include('devices.urls', namespace='device_form')),
     path('device_confirm_delete/', include('devices.urls', namespace='device_confirm_delete')),
     path('device_delete/', include('devices.urls', namespace='device_delete')),
