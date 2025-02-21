d407c07f8338f2e70c7e4e371e70a383c17f9054
IwonaAnna2024 <pinezzka@yahoo.com>
2025-02-21 21:08:09 +0100

    dashboard

--- DjangoProject/SCD_app/users/urls.py
+++ DjangoProject/SCD_app/users/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard),
-    path('signup', views.signup),
-    path('login', views.login),
-]
+    path('users/dashboard', views.dashboard),
+    path('log_out/', include('users.urls', namespace='log_out')),
+    path('change_password/', include('users.urls', namespace='change_password')),
+    path('device_form/', include('devices.urls', namespace='device_form')),
+    path('device_confirm_delete/', include('devices.urls', namespace='device_confirm_delete')),
+    path('device_delete/', include('devices.urls', namespace='device_delete')),
+    path('device_detail/', include('devices.urls', namespace='device_detail')),
+    path('device_list/', include('devices.urls', namespace='device_list')),
+    ]
--- DjangoProject/SCD_app/users/views.py
+++ DjangoProject/SCD_app/users/views.py
@@ -3,14 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def signup(request):
-    return render(request,'users/signup.html')
-
-#     if request.method == 'POST':
-#
-def login(request):
-    return render(request, 'users/login.html')
-
 def dashboard(request):
     return render(request, 'users/dashboard.html')
 
