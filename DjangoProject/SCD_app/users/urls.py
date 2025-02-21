53031b378da88da7b6cc51ef30ca78d4d92d6248
IwonaAnna2024 <pinezzka@yahoo.com>
2025-02-21 23:23:30 +0100

    Urls added

--- DjangoProject/SCD_app/users/urls.py
+++ DjangoProject/SCD_app/users/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.dashboard),
     # path('log_out/', log_out.views, name='logout'),
-    # path('change_password/', change_password.viws, namespace='change_password')),
+    # path('change_password/', change_password.views, namespace='change_password')),
     path('device_form/', include('devices.urls', namespace='device_form')),
     path('device_confirm_delete/', include('devices.urls', namespace='device_confirm_delete')),
     path('device_delete/', include('devices.urls', namespace='device_delete')),
