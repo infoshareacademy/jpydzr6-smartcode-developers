8c7c3fb453cbc222226cd1c27f42221c756cc91e
IwonaAnna2024 <pinezzka@yahoo.com>
2025-02-22 08:55:47 +0100

    Added views for editing devices

--- DjangoProject/SCD_app/users/views.py
+++ DjangoProject/SCD_app/users/views.py
@@ -4,7 +4,29 @@ from django.http import HttpResponse
 # Create your views here.
 
 def dashboard(request):
-    return render(request, 'users/dashboard.html')
+    return render(request, 'dashboard.html')
+
+def bulb_manager(request):
+    return render(request, 'bulb_manager.html')
+
+def curtain_manager(request):
+    return render(request, 'curtain_manager.html')
+
+def lawnmower_manager(request):
+    return render(request, 'lawnmower_manager.html')
+
+def plug_manager(request):
+    return render(request, 'plug_manager.html')
+
+
+def thermostat_manager(request):
+    return render(request, 'thermostat_manager.html')
+
+def weatherstation_manager(request):
+    return render(request, 'weatherstation_manager.html')
+
+
+
 
 
 
