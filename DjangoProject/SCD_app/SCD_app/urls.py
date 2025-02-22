73e1b9cb047d463927e7bd18a68dd2b6f112419a
mkruszyna <michal.kruszyna@gmail.com>
2025-02-22 19:46:54 +0100

    feat: added login_required in device views, fixes in password reset, change in base html in devices to be consistent with users app

--- DjangoProject/SCD_app/SCD_app/urls.py
+++ DjangoProject/SCD_app/SCD_app/urls.py
@@ -35,7 +35,9 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('accounts/password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/reset_password.html'), name='password_reset'),
-    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/reset_password_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/reset_password_done.html'), name='password_reset_complete'),
 
 # device
     path('devices/', views.devices, name='devices'),
